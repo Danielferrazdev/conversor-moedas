fa1a2a48113872f40292a025c1580a23bc86a6d0
Danielferrazdev <daniel.f.mendonca@gmail.com>
2025-03-30 23:50:37 -0300

    acrescentando pop-up para valores invalidos

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function converter() {
     let valoremreal = document.getElementById("valor").value
     let valordodia = 4
     
+    if (isNaN(valoremreal) || valoremreal <= 0) { 
+        /* isNaN - Impede que o usuário insira um valor inválido */
+        alert ("Por favor, digitar um valor válido em Real");
+        return;
+    }
+
     let valoremdolar = valoremreal / valordodia
 
     
